32afd30cdffdcc5713c6403f9e9564caf8f13281
Dino Zervos <dzervos@shulware.com>
2020-05-20 13:05:54 -0400

    Added control/app key

--- keyboards/dinokb1/keymaps/default/keymap.c
+++ keyboards/dinokb1/keymaps/default/keymap.c
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSPO,      KC_Z,    KC_X,    KC_C,  KC_V, KC_B,            \
   KC_LCTL,      KC_LGUI, KC_LALT, LT(PROG, KC_SPC),             \
   /* Right side */
-           KC_F5,   KC_F6,    KC_F7,          KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,       \
-           KC_6,    KC_7,     KC_8,           KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      \
-           KC_Y,    KC_U,     KC_I,           KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      \
-           KC_H,    KC_J,     KC_K,           KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                \
-  KC_RCTL, KC_N,    KC_M,                     KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC,      \
+                         KC_F5,   KC_F6,    KC_F7,          KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,       \
+                         KC_6,    KC_7,     KC_8,           KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      \
+                         KC_Y,    KC_U,     KC_I,           KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      \
+                         KC_H,    KC_J,     KC_K,           KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                \
+  MT(MOD_RCTL, KC_APP),  KC_N,    KC_M,                     KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC,      \
   LT(HOME, KC_SPC), OSM(MOD_LCTL | MOD_LALT), KC_RALT, KC_APP,  KC_CALC,          KC_RCTL,      \
   /* Number pad */
   KC_PSCR, KC_SLCK, KC_PAUS,          \
