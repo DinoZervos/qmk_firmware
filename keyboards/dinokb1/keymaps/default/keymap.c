12e6ff10b2da625d58a4eb7f7a6737c0da4f0ff8
Dino Zervos <dzervos@shulware.com>
2020-11-03 08:57:02 -0500

    Removed version info from file

--- keyboards/dinokb1/keymaps/default/keymap.c
+++ keyboards/dinokb1/keymaps/default/keymap.c
@@ -1,5 +1,4 @@
 /* Copyright 2017 Konstantinos Zervos
- * Version 6.1.1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
