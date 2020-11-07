8915bcae82c64adde1ab0841127d47100ae6fb06
Dino Zervos <dzervos@shulware.com>
2020-11-07 07:46:17 -0500

    Fixed extraneous space after newline slash

--- keyboards/dinokb1/keymaps/default/keymap.c
+++ keyboards/dinokb1/keymaps/default/keymap.c
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS, KC_TRNS, \
   KC_TRNS, LGUI(KC_Q), LGUI(KC_W), LGUI(KC_E), LGUI(KC_SPC), KC_TRNS,          \
   KC_TRNS, KC_TRNS,    KC_TRNS,    KC_F11,     KC_TRNS,      KC_TRNS,          \
-  KC_TRNS, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V),   KC_TRNS,          \  
+  KC_TRNS, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V),   KC_TRNS,          \
   KC_LALT, KC_TRNS,    KC_LGUI,    KC_TRNS,                                    \
   /* Right side */
             KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    \
@@ -184,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_INS,       KC_UP,    KC_PGUP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    \
             KC_HOME, KC_LEFT,      KC_DOWN,  KC_RIGHT, KC_END,  KC_TRNS, KC_TRNS,             \
   KC_TRNS,  KC_DEL,  LSFT(KC_DEL), KC_TRNS,  KC_PGDN,  KC_TRNS,          KC_TRNS,             \
-  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,                    KC_TRNS,             \
+  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,                    KC_TRNS,             \
   /* Number pad */
   KC_TRNS, KC_TRNS, KC_TRNS,          \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
